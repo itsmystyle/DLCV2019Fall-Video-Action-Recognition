34121cd929b20872a74b017ee14493431e730dea
itsmystyle <itsmystyle.0821@gmail.com>
2019-12-12 20:08:49 +0800

    moved full trainer to modules

--- modules/full_trainer.py
+++ modules/full_trainer.py
@@ -1,22 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
-import glob
-import pickle
-import sys
-sys.path.append("../")
 
-import numpy as np
-import pandas as pd
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import skimage
-# from tqdm.notebook import tqdm
+import torch.nn as nn
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
@@ -30,17 +16,13 @@ rescale_factor = 1.0
 random_seed = 42
 
 set_random_seed(random_seed)
-save_dir = "../models/Full_LRCNN/"
+save_dir = "models/Full_LRCNN/"
 full_path = "/tmp2/itsmystyle/h4_data/FullLengthVideos/"
-train_video_path = os.path.join(full_path, 'videos', 'train')
-train_label_path = os.path.join(full_path, 'labels', 'train')
-valid_video_path = os.path.join(full_path, 'videos', 'valid')
-valid_label_path = os.path.join(full_path, 'labels', 'valid')
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-# In[ ]:
-
+train_video_path = os.path.join(full_path, "videos", "train")
+train_label_path = os.path.join(full_path, "labels", "train")
+valid_video_path = os.path.join(full_path, "videos", "valid")
+valid_label_path = os.path.join(full_path, "labels", "valid")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # hyper-parameters
 epochs = 100
@@ -51,39 +33,45 @@ model = SeqRecurrentCNN()
 model.to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=5e-5)
-
 criterion = nn.NLLLoss()
-
 metric = MulticlassAccuracy()
-
 writer = SummaryWriter(os.path.join(save_dir, "train_logs"))
 
 
-# In[ ]:
+train_set = FullLengthVideosDataset(
+    train_video_path, train_label_path, length=80, overlap=20, sorting=True
+)
+valid_set = FullLengthVideosDataset(
+    valid_video_path, valid_label_path, length=100, overlap=0, sorting=True
+)
 
 
-train_set = FullLengthVideosDataset(train_video_path, train_label_path, length=80, overlap=20, sorting=True)
-valid_set = FullLengthVideosDataset(valid_video_path, valid_label_path, length=100, overlap=0, sorting=True)
-
-
-# In[ ]:
-
-
-train_loader = DataLoader(train_set, batch_size=4, num_workers=32, shuffle=True, collate_fn=train_set.collate_fn)
-valid_loader = DataLoader(valid_set, batch_size=4, num_workers=32, shuffle=False, collate_fn=valid_set.collate_fn)
-
-
-# In[ ]:
+train_loader = DataLoader(
+    train_set,
+    batch_size=4,
+    num_workers=32,
+    shuffle=True,
+    collate_fn=train_set.collate_fn,
+)
+valid_loader = DataLoader(
+    valid_set,
+    batch_size=4,
+    num_workers=32,
+    shuffle=False,
+    collate_fn=valid_set.collate_fn,
+)
 
 
 def _run_one_epoch(epoch, iters):
     model.train()
-    
-    trange = tqdm(enumerate(train_loader), total=len(train_loader), desc="Epoch {}".format(epoch))
-    
+
+    trange = tqdm(
+        enumerate(train_loader), total=len(train_loader), desc="Epoch {}".format(epoch)
+    )
+
     metric.reset()
     batch_loss = 0.0
-    
+
     for idx, batch in trange:
         iters += 1
 
@@ -94,7 +82,10 @@ def _run_one_epoch(epoch, iters):
         preds = model(frames, frames_len)
 
         # calculate loss and update weights
-        loss = criterion(preds.view(-1, preds.shape[-1]), labels.view(-1)) / accumulate_gradient
+        loss = (
+            criterion(preds.view(-1, preds.shape[-1]), labels.view(-1))
+            / accumulate_gradient
+        )
         if idx % accumulate_gradient == 0:
             optimizer.zero_grad()
         loss.backward()
@@ -107,7 +98,9 @@ def _run_one_epoch(epoch, iters):
         # update loss
         batch_loss += loss.item() * accumulate_gradient
         writer.add_scalars(
-            "Loss", {"iter_loss": loss.item(), "avg_loss": batch_loss / (idx + 1)}, iters,
+            "Loss",
+            {"iter_loss": loss.item(), "avg_loss": batch_loss / (idx + 1)},
+            iters,
         )
 
         # update tqdm
@@ -120,9 +113,6 @@ def _run_one_epoch(epoch, iters):
         optimizer.zero_grad()
 
     return batch_loss / (idx + 1), iters
-
-
-# In[ ]:
 
 
 def _eval_one_epoch(val_iters, best_accuracy):
@@ -166,16 +156,14 @@ def _eval_one_epoch(val_iters, best_accuracy):
             best_accuracy = metric.get_score()
             _loss = batch_loss / (idx + 1)
             torch.save(
-                model.state_dict(), os.path.join(
+                model.state_dict(),
+                os.path.join(
                     save_dir,
                     "model_best_{:.5f}_{:.5f}.pth.tar".format(best_accuracy, _loss),
-                )
+                ),
             )
 
     return batch_loss / (idx + 1), best_accuracy, val_iters
-
-
-# In[ ]:
 
 
 iters = -1
@@ -183,14 +171,7 @@ val_iters = -1
 best_accuracy = 0.0
 
 for epoch in range(epochs + 1):
-    
+
     loss, iters = _run_one_epoch(epoch, iters)
-    
+
     loss, best_accuracy, val_iters = _eval_one_epoch(val_iters, best_accuracy)
-
-
-# In[ ]:
-
-
-
-
